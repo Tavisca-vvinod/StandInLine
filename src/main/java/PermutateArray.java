32b0e7ba493ceda7079605f0a30ea64b8939f849
vvinod <vvinod@tavisca.in>
2019-08-07 17:38:58 +0530

    Removed user defined list to array conversion and used inbuild methods.

--- src/main/java/PermutateArray.java
+++ src/main/java/PermutateArray.java
@@ -3,12 +3,12 @@ import java.util.Arrays;
 import java.util.List;
 public class PermutateArray {
     public static void main(String[] args) {
-        System.out.println(Arrays.equals(test( new int[] {0,0,0,0,0}),(new int [] {1,2,3,4,5}))? "PASS":"FAIL");
-        System.out.println(Arrays.equals(test( new int[] {5,4,3,2,1,0}),(new int [] {6,5,4,3,2,1}))? "PASS":"FAIL");
-        System.out.println(Arrays.equals(test( new int[] {6,1,1,1,2,0,0}),(new int [] {6,2,3,4,7,5,1}))? "PASS":"FAIL");
-        System.out.println(Arrays.equals(test( new int[] {2,1,1,0}),(new int [] {4,2,1,3}))? "PASS":"FAIL");
+        System.out.println(Arrays.equals(test( new int[] {0,0,0,0,0}),(new Integer[] {1,2,3,4,5}))? "PASS":"FAIL");
+        System.out.println(Arrays.equals(test( new int[] {5,4,3,2,1,0}),(new Integer [] {6,5,4,3,2,1}))? "PASS":"FAIL");
+        System.out.println(Arrays.equals(test( new int[] {6,1,1,1,2,0,0}),(new Integer [] {6,2,3,4,7,5,1}))? "PASS":"FAIL");
+        System.out.println(Arrays.equals(test( new int[] {2,1,1,0}),(new Integer [] {4,2,1,3}))? "PASS":"FAIL");
     }
-    public static int[] test(int[]array){
+    public static Integer[] test(int[]array){
         List<List<Integer>> permutations = Permuter.permute(array);
         for(List<Integer> permutation:permutations)
         {
@@ -30,20 +30,10 @@ public class PermutateArray {
             }
             if(allClear==array.length)
             {
-                int[] permuttation=convertToArray(permutation);
-                return permuttation;
+                Integer[] soldierSequence = permutation.toArray(new Integer[permutation.size()]);
+                return soldierSequence;
             }
         }
         return null;
     }
-
-    private static int[] convertToArray(List<Integer> permutation) {
-        int[] permuttation=new int [permutation.size()];
-        for(int i=0;i<permutation.size();i++)
-        {
-            permuttation[i]=permutation.get(i);
-        }
-        return permuttation;
-    }
-
 }
